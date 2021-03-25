bcaba8d84190cc86a8ca46962fb0e6d495790043
edcrabtr <77771295+edcrabtr@users.noreply.github.com>
2021-03-24 20:54:55 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package edu.uncg;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GameInfoTranslator.getVideoGameInfo();
+        GenreTranslator.getGenres();
+    }
+}
