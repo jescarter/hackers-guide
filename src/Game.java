af9b0068c47bc0758bc3dcdb147f7bbd41c7a6cf
Ian Holder <iaholder@uncg.edu>
2021-03-30 14:24:28 -0400

    created Game object class

---
+++ src/Game.java
@@ -0,0 +1,16 @@
+package src;
+
+/**
+ * Create game objects with api data to be used for views
+ * last updated 03/30/2021
+ * Author(s) Ian Holder,
+ */
+
+public class Game {
+    String genre;
+    int genreID;
+    String title;
+    String[] tags;
+    int metacriticScore;
+    int playtime;
+}
